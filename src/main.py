d19ac3239c0ac4651cba080992d2f27ecd6460d3
Apeel Subedi <apeelsubedi@gmail.com>
2026-05-07 16:46:25 +0200

    feat: added book loader and vocan builder functions

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+data_dir  = ROOT / "Datasets"
+fig_dir   = ROOT / "figures"
+out_dir   = ROOT / "outputs"
+fig_dir.mkdir(exist_ok=True)
+out_dir.mkdir(exist_ok=True)
+
+def loadbook(filepath):
+    with open(filepath, 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+def VocabBuilder(data):
+    """it'll return sorted unique character dictionary and the reverse of it"""
+    unique_char = sorted(list(set(data)))
+    k = len(unique_char)
+    char2ind = {c:i for i, c in enumerate(unique_char)}
+    ind2char = {i:c for i, c in enumerate(unique_char)}
+    return unique_char, k, char2ind, ind2char
+
